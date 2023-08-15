4adc82fe83991984e6d0d6b303d64c24a149d922
USER <USER@DESKTOP-3Q2LMED>
2023-08-14 19:28:42 -0500

    Add project cvghfujdfuj.

--- HomeWorkA/NotValidNameException.cs
+++ HomeWorkA/NotValidNameException.cs
@@ -8,7 +8,7 @@ namespace HomeWorkA
 {
     class NotValidNameException : NotValidDataException
     {
-        public NotValidNameException():base("should be from 'a-z' or'A-Z' ,your input isn't valid")
+        public NotValidNameException():base("should be from 'a-z' or'A-Z' ,your input isn't valid!!!!")
         {
         }
     }
